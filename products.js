a18bf47245dff8a72ff0fbaa97814b07df0625af
vicnic <115088558+VirimaiSigni@users.noreply.github.com>
2025-06-24 16:33:01 +0200

    Update products.js

--- products.js
+++ products.js
@@ -1,4 +1,3 @@
-// Example product data
 const products = [
   {
     name: "Wireless Headphones",
@@ -23,7 +22,7 @@ const products = [
     description: "1080p HD webcam for video calls and streaming.",
     price: "$39.99",
     image: "https://e7.pngegg.com/pngimages/338/65/png-clipart-submersible-pump-water-well-manufacturing-faridabad-others-miscellaneous-company.png"
-  }
+  },
   {
     name: "HD Webcam",
     description: "1080p HD webcam for video calls and streaming.",
@@ -36,21 +35,19 @@ const products = [
     price: "$39.99",
     image: "https://e7.pngegg.com/pngimages/137/277/png-clipart-submersible-pump-product-water-solar-powered-pump-water-compressor-hydraulic-pump.png"
   },
-    
-      {
+  {
     name: "HD Webcam",
     description: "1080p HD webcam for video calls and streaming.",
     price: "$39.99",
     image: "https://thumbs.dreamstime.com/z/water-pump-submersible-pump-set-submersible-borehole-supplying-clean-water-great-depth-house-motor-water-pump-submersible-pump-set-242661550.jpg"
   },
-      {
+  {
     name: "HD Webcam",
     description: "1080p HD webcam for video calls and streaming.",
     price: "$39.99",
     image: "https://e7.pngegg.com/pngimages/343/164/png-clipart-submersible-pump-solar-powered-pump-water-pumping-irrigation-water-submersible-pump-borstelloze-elektromotor.png"
   }
 ];
-
 // Render the products
 const container = document.getElementById("products-container");
 
