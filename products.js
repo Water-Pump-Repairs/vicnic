87c0993c9e2d41afda9096a3c9d0089b54ef5254
vicnic <115088558+VirimaiSigni@users.noreply.github.com>
2025-06-24 16:50:33 +0200

    Update products.js

--- products.js
+++ products.js
@@ -1,49 +1,49 @@
 const products = [
   {
-    name: "Wireless Headphones",
-    description: "High-quality wireless headphones with noise cancellation.",
+    name: "Water Pump Repairs",
+    description: "Water Pump Repairs.",
     price: "$99.99",
     image: "https://3.imimg.com/data3/YV/VT/MY-2350301/pumps-industrial-500x500.jpg"
   },
   {
-    name: "Smart Watch",
-    description: "Track your fitness and stay connected with this stylish smartwatch.",
+    name: "Water Pump Repairs",
+    description: "Water Pump Repairs.",
     price: "$149.99",
     image: "https://e7.pngegg.com/pngimages/803/553/png-clipart-submersible-pump-water-well-pump-centrifugal-pump-pump-miscellaneous-axialflow-pump.png"
   },
   {
-    name: "Bluetooth Speaker",
-    description: "Portable Bluetooth speaker with deep bass and long battery life.",
+    name: "Water Pump Repairs",
+    description: "Water Pump Repairs.",
     price: "$59.99",
     image: "https://w7.pngwing.com/pngs/436/527/png-transparent-submersible-pump-wellers-hire-wastewater-water-submersible-drinking-water-submersible-pump.png"
   },
   {
-    name: "HD Webcam",
-    description: "1080p HD webcam for video calls and streaming.",
+    name: "Water Pump Repairs",
+    description: "Water Pump Repairs.",
     price: "$39.99",
     image: "https://e7.pngegg.com/pngimages/338/65/png-clipart-submersible-pump-water-well-manufacturing-faridabad-others-miscellaneous-company.png"
   },
   {
-    name: "HD Webcam",
-    description: "1080p HD webcam for video calls and streaming.",
+    name: "Water Pump Repairs",
+    description: "Water Pump Repairs.",
     price: "$39.99",
     image: "https://img.freepik.com/free-photo/deep-well-submersible-pump-isolated-white_627829-7499.jpg"
   },
   {
-    name: "HD Webcam",
-    description: "1080p HD webcam for video calls and streaming.",
+    name: "Water Pump Repairs",
+    description: "Water Pump Repairs.",
     price: "$39.99",
     image: "https://e7.pngegg.com/pngimages/137/277/png-clipart-submersible-pump-product-water-solar-powered-pump-water-compressor-hydraulic-pump.png"
   },
   {
-    name: "HD Webcam",
-    description: "1080p HD webcam for video calls and streaming.",
+    name: "Water Pump Repairs",
+    description: "Water Pump Repairs.",
     price: "$39.99",
     image: "https://thumbs.dreamstime.com/z/water-pump-submersible-pump-set-submersible-borehole-supplying-clean-water-great-depth-house-motor-water-pump-submersible-pump-set-242661550.jpg"
   },
   {
-    name: "HD Webcam",
-    description: "1080p HD webcam for video calls and streaming.",
+    name: "Water Pump Repairs",
+    description: "Water Pump Repairs.",
     price: "$39.99",
     image: "https://e7.pngegg.com/pngimages/343/164/png-clipart-submersible-pump-solar-powered-pump-water-pumping-irrigation-water-submersible-pump-borstelloze-elektromotor.png"
   }
